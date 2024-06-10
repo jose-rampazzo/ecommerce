da6f42bfb617ffefea114f20116630efedb470ee
jose-rampazzo <jh1997sl@gmail.com>
2024-06-10 15:44:04 -0300

    Login Admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,14 +9,20 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
-    public function __construct($opts = array(), $tpl_dir = "/views/") {
+    public function __construct($opts = array(), $tpl_dir = "/views/") 
+    {
+
+        #$this->defaults["data"]["session"] = $_SESSION;
 
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
+            "base_url"      => null,
             "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false
@@ -28,8 +34,8 @@ class Page {
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
-
+        if ($this->options["header"] === true) $this->tpl->draw("header"); 
+        
     }
 
     private function setData($data = array())
@@ -49,14 +55,12 @@ class Page {
 
     }
 
-    public function __destruct() {
+    public function __destruct() 
+    {
 
-        $this->tpl->draw("footer");
-
-
+        if ($this->options["header"] === true) $this->tpl->draw("footer");
 
     }
-
 
 }
 
